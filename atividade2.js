c39a310f4d7f603178af96523d84372b2b913c7e
Flavio Santos <flavio.dronov@gmail.com>
2023-05-05 21:06:48 -0300

    Adiciona Atividade2

---
+++ atividade2.js
@@ -0,0 +1,25 @@
+import PromptSync from "prompt-sync"
+const prompt =  PromptSync()
+
+const clientes = []
+
+while(true){
+    const nome = prompt('Insira o Nome do Cliente: ')
+    if (nome === ''){
+        break;
+    }
+
+    const hobbies = []
+    while (true) {
+        const hobby = prompt(`Insira um hobby para ${nome}: `)
+        if (hobby === ''){
+            break
+        }
+        hobbies.push(hobby);
+    }
+
+    clientes[nome] = hobbies;
+}
+
+console.log("Clientes e seus hobbies: ")
+console.log(clientes)
